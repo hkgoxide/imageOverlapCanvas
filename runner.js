6a8896aa13ad0eba60d72b7ac0f3f5826a5fde83
hkgoxide <hkgoxide@gmail.com>
2016-11-27 01:53:52 +0800

    Update runner.js

--- runner.js
+++ runner.js
@@ -128,7 +128,7 @@ function drawFile(img){
 	result = lab2rgb(merge(rgb2lab(pixsrc),rgb2lab(pix)));
 	// Draw the ImageData object.
 	};
-	srcImage.onload()= function(){
+	srcImage.onload= function(){
   		image.src=img;
 	};
   	srcImage.src = "source.jpg";
