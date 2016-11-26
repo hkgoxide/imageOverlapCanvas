23f0aa50acba30c00af8274c8b2e0d663fe7a8a9
hkgoxide <hkgoxide@gmail.com>
2016-11-27 01:47:49 +0800

    Update runner.js

--- runner.js
+++ runner.js
@@ -4,7 +4,7 @@ function previewFile() {
   var reader  = new FileReader();
 
   reader.addEventListener("load", function () {
-    preview.src = reader.result;
+    //preview.src = reader.result;
     drawFile(reader.result);
   }, false);
 
@@ -13,49 +13,123 @@ function previewFile() {
   }
 }
 
+/*
+Input an image data object
+*/
+function rgb2lab(img,callback){
+	var r1 = [[0.3811,0.5783,0.0402],[0.1967,0.7244,0.0782],[0.0241,0.1288,0.8444]];
+	var r2 = [[0.5774,0.5774,0.5774],[0.4082,0.4082,-0.8165],[0.7071,-0.7071,0]];
+	var img_lms = [[],[],[]];
+	var img_lab = [[],[],[]];
+	var img_rgb = [[],[],[]];
+	for (var i=0,n=img.length; i < n; i += 4){
+		//R
+		img_rgb[0].push(img[i]/255);
+		//G
+		img_rgb[1].push(img[i+1]/255);
+		//B
+		img_rgb[2].push(img[i+2]/255);
+	}
+	for(var i = 0, n = img.length; i < n; i += 4){
+		//channel log-L
+		img_lms[0].push(Math.max(Math.log(r1[0][0]*img_rgb[0][i] + r1[0][1]*img_rgb[1][i] + r1[0][2]*img_rgb[2][i]),1e-25));
+		//channel log-M
+		img_lms[1].push(Math.max(Math.log(r1[1][0]*img_rgb[0][i] + r1[1][1]*img_rgb[1][i] + r1[1][2]*img_rgb[2][i]),1e-25));
+		//Channel log-S
+		img_lms[2].push(Math.max(Math.log(r1[2][0]*img_rgb[0][i] + r1[2][1]*img_rgb[1][i] + r1[2][2]*img_rgb[2][i]),1e-25));
+	}
+	delete img_rgb;
+	//To CIE-Lab
+	for(var i = 0, n = img_lms[0].length; i < n; i++){
+		    img_lab[0].push(r2[0][0]*img_lms[0][i] + r2[0][1]*img_lms[1][i] + r2[0][2]*img_lms[2][i]);
+		    img_lab[1].push(r2[1][0]*img_lms[0][i] + r2[1][1]*img_lms[1][i] + r2[1][2]*img_lms[2][i]);
+		    img_lab[2].push(r2[2][0]*img_lms[0][i] + r2[2][1]*img_lms[1][i] + r2[2][2]*img_lms[2][i]);
+	}
+	if(callback){
+		callback();
+	}
+	delete img_lms;
+	return img_lab;
+}
+/*1 is source, 2 is target*/
+function mergeImg(img_lab1,img_lab2){
+	var mean_imglab1 = [math.mean(img_lab1[0]),math.mean(img_lab1[1]),math.mean(img_lab1[2])];
+	var mean_imglab2 = [math.mean(img_lab2[0]),math.mean(img_lab2[1]),math.mean(img_lab2[2])];
+	var std_imglab1 = [math.std(img_lab1[0]),math.std(img_lab1[1]),math.std(img_lab1[2])];
+	var std_imglab2 = [math.std(img_lab2[0]),math.std(img_lab2[1]),math.std(img_lab2[2])];
+	var img_result=[[],[],[]];
+	for (var i = 0, n = mean_imglab1[0].length; i < n; i++){
+		img_result[0].push(img_lab1[0][i]-mean_imglab1[0])*(std_imglab2[0]/std_imglab1[0])+mean_imglab2[0]);
+		img_result[1].push(img_lab1[1][i]-mean_imglab1[1])*(std_imglab2[1]/std_imglab1[1])+mean_imglab2[1]);
+		img_result[2].push(img_lab1[2][i]-mean_imglab1[2])*(std_imglab2[2]/std_imglab1[2])+mean_imglab2[2]);
+	}
+	return img_result;
+}
+
+function lab2rgb(img_lab,callback){
+	var inv_r2 =[[0.5774,0.4082,0.7071],[0.5774,0.4082,-0.7071],[0.5774,-0.8165,0.0000]];
+	var inv_r1 =[[4.4687,-3.5887,0.1196],[-1.2197,2.3831,-0.1626],[0.0585,-0.2611,1.2057]];
+	var img_lms = [[],[],[]];
+	var img_rgb = [[],[],[]];
+	var pixd = [];
+	for(var i = 0, n = img_lab[0].length; i < n; i++){
+		    img_lms[0].push(Math.exp(inv_r2[0][0]*img_lab[0][i] + inv_r2[0][1]*img_lab[1][i] + inv_r2[0][2]*img_lab[2][i]));
+		    img_lms[1].push(Math.exp(inv_r2[1][0]*img_lab[0][i] + inv_r2[1][1]*img_lab[1][i] + inv_r2[1][2]*img_lab[2][i]));
+		    img_lms[2].push(Math.exp(inv_r2[2][0]*img_lab[0][i] + inv_r2[2][1]*img_lab[1][i] + inv_r2[2][2]*img_lab[2][i]));
+	}
+	for(var i = 0, n = img_lms[0].length; i < n; i++){
+		    img_rgb[0].push(inv_r1[0][0]*img_lms[0][i] + inv_r1[0][1]*img_lms[1][i] + inv_r1[0][2]*img_lms[2][i]);
+		    img_rgb[1].push(inv_r1[1][0]*img_lms[0][i] + inv_r1[1][1]*img_lms[1][i] + inv_r1[1][2]*img_lms[2][i]);
+		    img_rgb[2].push(inv_r1[2][0]*img_lms[0][i] + inv_r1[2][1]*img_lms[1][i] + inv_r1[2][2]*img_lms[2][i]);
+	}
+	for(var i = 0,n=img_lms[0].length; i < n; i++){
+		pixd.push(Math.round(img_rgb[0][i]*255));
+		pixd.push(Math.round(img_rgb[1][i]*255));
+		pixd.push(Math.round(img_rgb[2][i]*255));
+		pixd.push(255);
+	}
+	if(callback){
+		callback();
+	}
+	return pixd;
+}
+
 function drawFile(img){
 	var image = new Image();
-	  image.onload = function(){
+	var srcImage = new Image();
+	image.onload = function(){
 		//Get canvas
 		var canvas = document.getElementById('myCanvas');
 		var context = canvas.getContext('2d');
+		var srcCanvas = document.getElementById('srcCanvas');
+		var srcContext = srcCanvas.getContext('2d');
 		//Resize canvas to image size
 		context.canvas.width = image.width;
 		context.canvas.height = image.height;
 		//Edit the image
-		 var x = 0, y = 0;
-
-		    // Draw the image on canvas.
-		    context.drawImage(this, x, y);
-
-		    // Get the pixels.
-		    var imgd = context.getImageData(x, y, this.width, this.height);
-		    var pix = imgd.data;
-
-		    // Loop over each pixel and invert the color.
-		    for (var i = 0, n = pix.length; i < n; i += 4) {
-		      pix[i  ] = 255 - pix[i  ]; // red
-		      pix[i+1] = 255 - pix[i+1]; // green
-		      pix[i+2] = 255 - pix[i+2]; // blue
-		      // i+3 is alpha (the fourth element)
+		var x = 0, y = 0;
+		// Draw the image on canvas.
+		srcContext.drawImage(srcImage,x,y);
+		context.drawImage(this, x, y);
+		// Get the pixels.
+		var srcimgd = srcContext.getImageData(x,y,srcImage.width,srcImage.height);
+		var imgd = context.getImageData(x, y, this.width, this.height);
+		var pixsrc = srcimgd.data;
+		var pix = imgd.data;
+		var result = undefined;
+		var intervalID = setInterval(function(){
+		    if(result){
+			imgd.data = result;
+			context.putImageData(imgd, x, y);
+			clearInterval(intervalID);
+			break;
 		    }
-
-		    // Draw the ImageData object.
-		    context.putImageData(imgd, x, y);
-// 		var data = context.getImageData(x, y, 1, 1).data;
-// 		var color = new Color([data[0], data[1], data[2]]);
-// 		context.drawImage(image,0,0,imageWidth,imageHeight);
-	  }
-  
-  	image.src=img;
-}
-
-/*
-Input an image object
-*/
-function lab2rgb(img,callback){
-	
-	if(callback){
-		callback();
+	});
+	result = lab2rgb(merge(rgb2lab(pixsrc),rgb2lab(pix)));
+	// Draw the ImageData object.
 	}
+	srcImage.onload(){
+  		image.src=img;
+	}
+  	srcImage.src = "source.jpg";
 }
